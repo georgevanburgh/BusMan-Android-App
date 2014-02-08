e888fbe8e1c1349f2a8d2dd52364398f20907d16
Richard de Mellow <richard.demellow@student.manchester.ac.uk>
2014-02-08 13:23:59 +0000

    adding api libs

---
+++ src/com/intrivix/android/busman/MapPane.java
@@ -0,0 +1,27 @@
+package com.intrivix.android.busman;
+import android.app.Activity;
+import android.os.Bundle;
+//import android.gms.maps.*;
+
+public class MapPane extends Activity{
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.map_activity);
+
+        // Get a handle to the Map Fragment
+       // GoogleMap map = ((MapFragment) getFragmentManager()
+       //         .findFragmentById(R.id.map)).getMap();
+//
+      //  LatLng sydney = new LatLng(-33.867, 151.206);
+
+     //   map.setMyLocationEnabled(true);
+     //map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+//
+     //   map.addMarker(new MarkerOptions()
+      //          .title("Sydney")
+      //          .snippet("The most populous city in Australia.")
+       //         .position(sydney));
+    }
+
+}
