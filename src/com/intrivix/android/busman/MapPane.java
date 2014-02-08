b74c8cbec048579a2c2ebb863e81acb83d67b1a6
eNcoRe101 <cricket-rocks@hotmail.co.uk>
2014-02-08 19:19:03 +0000

    added traffic things

--- src/com/intrivix/android/busman/MapPane.java
+++ src/com/intrivix/android/busman/MapPane.java
@@ -1,30 +1,45 @@
 package com.intrivix.android.busman;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
-
 public class MapPane extends Activity {
+	long timeLastUpdated = 0;
+	final long UPDATE_INTERVAL = 2000;
+	final long MAX_UPDATE_INTERVAL = 20000;
+	String lastUpdatedProviderType = "";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
 
-        // Get a handle to the Map Fragment
-        GoogleMap map = ((MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map)).getMap();
+		// Acquire a reference to the system Location Manager
+		LocationManager locationManager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
+		
+		Location loc=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		
+		// Get a handle to the Map Fragment
+				GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(
+						R.id.map)).getMap();
+				LatLng sydney = new LatLng(loc.getLongitude(),
+						loc.getLatitude());
 
-        LatLng sydney = new LatLng(-33.867, 151.206);
+				map.setMyLocationEnabled(true);
+				map.setTrafficEnabled(true);
+				map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
 
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+				map.addMarker(new MarkerOptions().title("You!")
+						.snippet("You are here (Well we think :P").position(sydney));
 
-        map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
-    }
+		
 
+	}
 }
