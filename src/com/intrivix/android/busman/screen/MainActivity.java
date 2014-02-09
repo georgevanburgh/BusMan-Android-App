dfd296494aadc8edc3cec649c57732ec1d7f57e6
Devin Ridgway <devinr@dobango.com>
2014-02-09 08:52:44 +0000

    added some location functionality for MainActivity

--- src/com/intrivix/android/busman/screen/MainActivity.java
+++ src/com/intrivix/android/busman/screen/MainActivity.java
@@ -16,6 +16,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -170,6 +173,39 @@ public class MainActivity extends Activity {
         mDrawerList.setItemChecked(position, true);
         mCurrentFragment = position;
         mDrawerLayout.closeDrawer(mDrawerList);
+        
+        setupLocationListeners();
+    }
+    
+    private void setupLocationListeners() {
+    	// Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+            	// Called when a new location is found by the network location provider.
+            	
+            	if(System.currentTimeMillis() - timeLastUpdated > UPDATE_INTERVAL) {
+            		if(lastUpdatedProviderType.equals(location.getProvider())
+            				|| location.getProvider().equals(LocationManager.GPS_PROVIDER)
+            				|| System.currentTimeMillis() - timeLastUpdated > MAX_UPDATE_INTERVAL)
+            		updateEstimatedLocation(location);
+            		timeLastUpdated = System.currentTimeMillis();
+            		lastUpdatedProviderType = location.getProvider();
+            	}
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+          };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 
     @Override
